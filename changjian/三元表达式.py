78af344acba280952e0a76634a2f1d1eb16678e1
wangmengmeng <wmm_0165@163.com>
2020-06-08 18:09:27 +0800

    三元表达式、字符串大小写问题

---
+++ changjian/三元表达式.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/6/8 15:51
+# @Author : wangmengmeng
+
+a = -1
+express = 1 if a > 0 else 3
+print(express)
---
+++ changjian/如何将字符串转为小写.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/6/8 16:01
+# @Author : wangmengmeng
+
+"""
+lower() 小写
+upper() 大写
+"""
+
+
+ss = 'acbvmm'
+upss = ss.upper()
+print(upss)
+print(ss.isupper())
